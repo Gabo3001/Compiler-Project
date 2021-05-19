9bc900617a04ad9d08912e479a4dbe1331053aef
gabo3001 <A01176807@itesm.mx>
2021-05-19 01:37:39 -0500

    Add new function to access values in quadruple

--- ObjQuad.py
+++ ObjQuad.py
@@ -9,3 +9,19 @@ class Quadruple:
   #Funion hash, crea un index unico dentro de nuestro arreglo a partir de una llave
   def get_quad(self):
       return [self.op, self.op_izq, self.op_der, self.temp]
+
+  #Funciton that return the operator of the quadruple
+  def getOp(self):
+    return self.op
+
+  #Funciton that return the left operando of the quadruple
+  def getOpIzq(self):
+    return self.op_izq
+
+  #Funciton that return the right operando of the quadruple
+  def getOpDer(self):
+    return self.op_der
+
+  #Funciton that return the temporal operator of the quadruple
+  def getTemp(self):
+    return self.temp
